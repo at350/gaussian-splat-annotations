7b6cb36bf26756a044c5570cadcb23b0b539daa0
Alan Tai <46613976+at350@users.noreply.github.com>
2025-11-05 22:28:28 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-  base: './gaussian-splat-annotations/',
+  base: '/gaussian-splat-annotations/',
   build: { target: 'es2020' },           // good defaults
   server: {
     headers: {
